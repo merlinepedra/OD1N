8bad12fb4d9b5eb4f7f1c41c65a46fa0c3102e77
cooler <cooler@darkstar.night>
2014-08-30 04:21:04 -0300

    old escape html

--- html_entities.h
+++ html_entities.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include "mem_ops.h"
 
+void* my_alloc(void* ptr, unsigned sz);
 int get_entity(char c); 
 char* html_entities(char* text); 
